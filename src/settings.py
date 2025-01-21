0c1c21987df07e230c48d43a399334096c29d399
raphael apeh <raphaelapeh37@gmail.com>
2025-01-21 20:15:21 +0100

    modify the settings DEFAULT_FROM_EMAIL

--- src/settings.py
+++ src/settings.py
@@ -124,6 +124,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+DEFAULT_FROM_EMAIL = config("EMAIL", cast=str)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
