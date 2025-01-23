89891c93c91aaf3929629f04f1f60938f96d5691
raphael apeh <raphaelapeh37@gmail.com>
2025-01-23 23:21:24 +0100

    getting started for deployment

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
+#!/usr/bin/env bash
+# Exit on error
 set -o errexit
 
+# Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
-python manage.py collectstatic --oninput
+# Convert static asset files
+python manage.py collectstatic --no-input
 
+# Apply any outstanding database migrations
 python manage.py makemigrations
-
 python manage.py migrate
 
 python hello.py
