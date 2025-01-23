b976f8e06b88c80436f70960b132c2193ca78f0a
raphael apeh <raphaelapeh37@gmail.com>
2025-01-23 21:27:41 +0100

    improving the code

--- checkouts/management/commands/sync_products.py
+++ checkouts/management/commands/sync_products.py
@@ -5,7 +5,7 @@ from django.core.management.base import BaseCommand
 
 from products.models import Product
 
-#stripe.api_key = settings.STRIPE_SECRET_KEY
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
 class Command(BaseCommand):
@@ -16,8 +16,11 @@ class Command(BaseCommand):
             for product in Product.objects.all():
                 if all([product.stripe_product_id, product.stripe_price_id]):
                     continue
+                
                 stripe_product_id = stripe.Product.create(name=product.product_name, description=product.product_description).id
+                
                 stripe_price_id = stripe.Price.create(product=stripe_product_id, currency="usd", unit_amount=int(product.price) * 100).id
+                
                 product.stripe_product_id = stripe_product_id
                 product.stripe_price_id = stripe_price_id
                 product.save()
